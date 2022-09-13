931a432536136c48fcf27f44246d5e3b6a9f4ca8
Chisom Onyi <onyic80@gmail.com>
2022-09-13 14:24:21 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char *sh = "putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
+
