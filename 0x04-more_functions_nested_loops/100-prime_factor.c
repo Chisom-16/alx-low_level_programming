3b1a69da5cd76af7d4b68800bf941e6bd0e75575
Chisom Onyi <onyic80@gmail.com>
2022-09-15 18:48:21 -0700

    my commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
+ * main - Finds and prints the largest prime factor
+ * of the number 612852475143
+ * Return: 0 if success
  *
  */
 int main(void)
 {
+	unsigned long num = 612852475143;
+	unsigned long factor = 2;
+
+	while (num)
+	{
+		if (num / factor > 0 && num % factor == 0)
+			num /= factor;
+		else if (num / factor > 0 && num % factor != 0)
+			factor++;
+		else
+			break;
+	}
+	printf("%ld\n", factor);
 	return (0);
 }
