77c624973effaba683b00611c9c9e6edcd8cf4f2
Chisom Onyi <onyic80@gmail.com>
2022-09-20 16:46:05 -0700

    my commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of a and b
+ * swap_int -function that swaps two values of integers
  * @a: int 1
  * @b: int 2
  * Return: 0
  */
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
+	int n;
 
+	n = *a;
 	*a = *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	*b = n;
 }
