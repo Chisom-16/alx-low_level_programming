591719938c7d5d176d147f0c7c363aea07e8e123
Chisom Onyi <onyic80@gmail.com>
2022-09-20 14:50:48 -0700

    my commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,7 +5,6 @@
  * Return: string
  *
  */
-
 void _puts(char *str)
 {
 	int i;
@@ -13,6 +12,6 @@ void _puts(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
