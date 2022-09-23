cebe740eb6a7e756ac1c7de4b0a56a8c69f2fd43
Chisom Onyi <onyic80@gmail.com>
2022-09-22 19:17:19 -0700

    my commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	 /**
-	  * write your line of code here...
+	  * entry point: appends src to dest
 	  * Remember:
 	  * - you are not allowed to use a
 	  * - you are not allowed to modify p
